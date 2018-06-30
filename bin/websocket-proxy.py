1eeb9ca660d0d3e7eb2e21bb83f07ad12390939c
Craig Pratt <craig@ecaspia.com>
2018-06-29 19:01:10 -0700

    Refactored the proxy to put more logic in the WSClient
    In preparation for adding stale connection detection (ws ping/pong)

--- bin/websocket-proxy.py
+++ bin/websocket-proxy.py
@@ -11,6 +11,8 @@ import sys
 import pathlib
 import ssl
 import configparser
+
+from threading import Timer
 from quart import json
 
 bin_path = pathlib.Path (__file__).parent
@@ -28,70 +30,128 @@ root_cert_path = bin_path.parent.joinpath ('lib/micronets-ws-root.cert.pem')
 meetup_table = {}
 
 class WSClient:
-    def __init__ (self, meetup_id, websocket, hello_message=None):
-        self.peer_arrival_future = asyncio.get_event_loop ().create_future ()
+    def __init__ (self, meetup_id, websocket, hello_message=None, peer_client=None, ping_timeout_limit=10):
         self.meetup_id = meetup_id
         self.websocket = websocket
         self.hello_message = hello_message
-        self.peer_arrival_future = asyncio.get_event_loop ().create_future ()
+        self.peer_arrival_future = None
+        self.ping_timeout_limit_s = ping_timeout_limit
+        self.ping_timeout_timer = None
+        self.hello_message = hello_message
+        self.peer_client = peer_client
+        self.start_ping_timer ()
 
+    async def recv_hello_message (self):
+        raw_message = await self.websocket.recv ()
+        message = json.loads (raw_message)
+        if (not message):
+            raise Exception (f"message does not appear to be json")
+        hello_message = check_json_field (message, 'message', dict, True)
+        message_id = check_json_field (hello_message, 'messageId', int, True)
+        message_type = hello_message ['messageType']
+        requires_response = check_json_field (hello_message, 'requiresResponse', bool, True)
+        if (not message_type == "CONN:HELLO"):
+            raise Exception (f"message does not appear to be a CONN:HELLO message (found {message_type})")
+        self.hello_message = message
+        return self.hello_message
+
+    async def get_hello_message (self):
+        return self.hello_message
+    
+    async def wait_for_peer (self):
+        self.peer_arrival_future = asyncio.get_event_loop ().create_future ()
+        recv_task = asyncio.get_event_loop ().create_task (self.websocket.recv ())
+        logger.info (f"ws_client {id (self)}: wait_for_peer: waiting for peer on {self.meetup_id}...")
+        # We need to have a call to recv() in order to know if the socket closes while we're waiting
+        done, pending = await asyncio.wait( [self.peer_arrival_future, recv_task],
+                                            return_when=asyncio.FIRST_COMPLETED)
+        if (not self.peer_arrival_future in done):
+            raise Exception (f"Client {id (self)} failed/recv-ed data before peer could attach to {path}")
+        if (recv_task.done ()):
+            initial_message = recv_task.result ()
+            if (initial_message):
+                # A message slipped through
+                logger.warn (f"ws_client {id (self)}: wait_for_peer: A message was received while waiting for a peer - DROPPING: {message}")
+        else:
+            recv_task.cancel ()
+        self.peer_client = self.peer_arrival_future.result ()
+        self.peer_arrival_future = None
+        return self.peer_client
+
+    def set_peer (self, peer):
+        if (self.peer_arrival_future):
+            self.peer_arrival_future.set_result (peer)
+        self.peer_client = peer
+
+    def start_ping_timer (self):
+        if (self.ping_timeout_limit_s and self.ping_timeout_limit_s > 0):
+            if self.ping_timeout_timer:
+                self.ping_timeout_timer.cancel ()
+            self.ping_timeout_timer = Timer (self.ping_timeout_limit_s, self.ping_timeout)
+            self.ping_timeout_timer.start ()
+
+    def ping_timeout (self):
+        logger.warn (f"ws_client {id (self)}: ping timeout")
+
+    async def clean_up (self):
+        if (self.websocket.open):
+            await self.websocket.close (reason=f"The peer connection from {self.websocket.remote_address} closed")
+
+    async def send_message (self, message):
+        return await self.websocket.send (message)
+
+    async def relay_messages_to_peer (self):
+        logger.debug (f"ws_client {id (self)}: relay_messages_to_peer: sending cached hello to client {id (self.peer_client)}")
+        logger.debug ("        ", self.hello_message)
+        await self.peer_client.send_message (json.dumps (self.hello_message))
+        logger.info (f"ws_client {id (self)}: relay_messages_to_peer: Routing all messages to {id (self.peer_client)}")
+        while True:
+            message = await self.websocket.recv ()
+            logger.info (f"ws_client {id (self)}: relay_messages_to_peer: Copying message to client {id (self.peer_client)}")
+            logger.debug (message)
+            await self.peer_client.send_message (message)
+        
 async def ws_connected (websocket, path):
     try:
         remote_address = websocket.remote_address
-        logger.info (f"ws_connected: from {websocket.remote_address}, {path}")
+        logger.info (f"ws_connected: from {remote_address}, {path}")
         if (not path.startswith (proxy_service_prefix)):
             logger.warn (f"ws_connected: Unsupported path: {proxy_service_prefix} - CLOSING!")
             return
         meetup_id = path [len (proxy_service_prefix):]
+        if (not meetup_id in meetup_table):
+            client_list = []
+            meetup_table [meetup_id] = client_list
+        else:
+            client_list = meetup_table [meetup_id]
+        if (len (client_list) >= 2):
+            logger.warn (f"ws_connected: client {id (new_client)}: meetup ID {meetup_id} "
+                         f"already has {len (client_list)} clients - CLOSING connection from {remote_address}.")
+            return
+
         new_client = WSClient (meetup_id, websocket)
+        client_list.append (new_client)
+
         logger.debug (f"ws_connected: client {id (new_client)}: (meetup_id: {meetup_id})")
         logger.debug (f"ws_connected: client {id (new_client)}: Waiting for HELLO message...")
-        hello_message = await get_hello_message (websocket)
+        hello_message = await new_client.recv_hello_message ()
         logger.info (f"ws_connected: client {id (new_client)}: Received HELLO message:")
         logger.info (json.dumps (hello_message, indent=2))
-        new_client.hello_message = hello_message
-    except ConnectionClosed as cce:
-        logger.info (f"ws_connected: client {id (new_client)} disconnected")
-    except Exception as ex:
-        logger.warn (f"ws_connected: client {id (new_client)}: Caught an exception processing hello message: {ex}")
 
-    if (not meetup_id in meetup_table):
-        client_list = []
-        meetup_table [meetup_id] = client_list
-    else:
-        client_list = meetup_table [meetup_id]
+        # Here's where we'd add any accept criteria based on the HELLO message
 
-    initial_message = None
-    if (len (client_list) == 0):
-        logger.debug (f"ws_connected: client {id (new_client)} is the first connected to {path}")
-        client_list.append (new_client)
-        logger.info (f"ws_connected: client {id (new_client)} - waiting for peer on {path}...")
-        recv_task = asyncio.get_event_loop ().create_task (websocket.recv ())
-        # We need to have a call to recv() in order to know if the socket closes while we're waiting
-        done, pending = await asyncio.wait( [new_client.peer_arrival_future, recv_task],
-                                            return_when=asyncio.FIRST_COMPLETED)
-        if (not new_client.peer_arrival_future in done):
-            raise Exception (f"Client {id (new_client)} failed/recved data before peer could attach to {path}")
-        if (recv_task.done ()):
-            # A message slipped through
-            initial_message = recv_task.result ()
+        if (len (client_list) == 1):
+            logger.debug (f"ws_connected: client {id (new_client)} is the first connected to {path}")
+            await new_client.wait_for_peer ()
         else:
-            recv_task.cancel ()
-        peer_client = new_client.peer_arrival_future.result ()
-        logger.info (f"ws_connected: client {id (new_client)}: peer {id (peer_client)} connected via {path}")
-    else:
-        if (len (client_list) > 1):
-            logger.warn (f"ws_connected: client {id (new_client)}: meetup ID {meetup_id} "
-                         f"already has {len (client_list)} clients - CLOSING!")
-            return
-        client_list.append (new_client)
-        peer_client = client_list [0]
-        logger.debug (f"ws_connected: client {id (new_client)}: signalling waiting peer {id (peer_client)}")
-        peer_client.peer_arrival_future.set_result (new_client)
+            logger.debug (f"ws_connected: client {id (new_client)} is the second connected to {path}")
+            peer_client = client_list [0]
+            new_client.set_peer (peer_client)
+            peer_client.set_peer (new_client)
 
-    logger.debug (f"ws_connected: client {id (new_client)}: Starting ws_reader()...")
-    try:
-        await ws_reader (new_client, peer_client, initial_message)
+        # Will just relay until someone disconnects...
+        await new_client.relay_messages_to_peer ()
+        logger.info (f"ws_connected: client {id (new_client)} relay_messages_to_peer() returned")
     except websockets.ConnectionClosed as cce:
         logger.info (f"ws_connected: client {id (new_client)} disconnected")
     except Exception as Ex:
@@ -100,37 +160,8 @@ async def ws_connected (websocket, path):
     finally:
         logger.info (f"ws_connected: client {id (new_client)}: Cleaning up...")
         client_list.remove (new_client)
-        if (peer_client.websocket.open):
-            await peer_client.websocket.close (reason=f"The peer connection from {remote_address} closed")
+        await new_client.clean_up ()
     # When this function returns, the websocket is closed
-
-async def get_hello_message (websocket):
-    raw_message = await websocket.recv ()
-    message = json.loads (raw_message)
-    if (not message):
-        raise Exception (f"message does not appear to be json")
-    hello_message = check_json_field (message, 'message', dict, True)
-    message_id = check_json_field (hello_message, 'messageId', int, True)
-    message_type = hello_message ['messageType']
-    requires_response = check_json_field (hello_message, 'requiresResponse', bool, True)
-    if (not message_type == "CONN:HELLO"):
-        raise Exception (f"message does not appear to be a CONN:HELLO message (found {message_type})")
-    return message
-
-async def ws_reader (source_client, dest_client, initial_message):
-    logger.debug (f"ws_reader: client {id (source_client)}: sending cached hello to client {id (dest_client)}")
-    logger.debug ("        ", source_client.hello_message)
-    await dest_client.websocket.send (json.dumps (source_client.hello_message))
-    logger.info (f"ws_reader: client {id (source_client)}: Routing all messages to {id (dest_client)}")
-    while True:
-        if (initial_message):
-            message = initial_message
-            initial_message = None
-        else:
-            message = await source_client.websocket.recv ()
-        logger.info (f"ws_reader: client {id (source_client)}: Copying message to client {id (dest_client)}")
-        logger.debug (message)
-        await dest_client.websocket.send (message)
 
 def check_json_field (json_obj, field, field_type, required):
     '''Thrown an Exception of json_obj doesn't contain field and/or it isn't of type field_type'''
